781ab6c4193d6df74ed2bc5488dc8f2390d42655
Viktor Beresnev <112702039+VvVitekVvV@users.noreply.github.com>
2024-11-27 17:23:25 +0300

    Implemented Engine script which bind all blocks. Modified STT and NER blocks for using (#27)
    Co-authored-by: DanilKuznetcov <dankuziur@gmail.com>

---
+++ Engine.py
@@ -0,0 +1,63 @@
+import STT
+import NER
+import TTS
+
+class Engine:
+
+    def __init__(self):
+        self.listener = STT.Listener()
+        self.parser = NER.NER_parser()
+        self.vocalize = TTS.vocalize
+
+        self.status = {
+            'request' : None,
+            'doctor' : None, 
+            'date' : None, 
+            'time'  : None, 
+            # 'new_date' : None, 
+            # 'new_time' : None,
+        }
+
+    def date_to_words(self, date):
+        day_list = ['первое', 'второе', 'третье', 'четвёртое',
+            'пятое', 'шестое', 'седьмое', 'восьмое',
+            'девятое', 'десятое', 'одиннадцатое', 'двенадцатое',
+            'тринадцатое', 'четырнадцатое', 'пятнадцатое', 'шестнадцатое',
+            'семнадцатое', 'восемнадцатое', 'девятнадцатое', 'двадцатое',
+            'двадцать первое', 'двадцать второе', 'двадцать третье',
+            'двадацать четвёртое', 'двадцать пятое', 'двадцать шестое',
+            'двадцать седьмое', 'двадцать восьмое', 'двадцать девятое',
+            'тридцатое', 'тридцать первое']
+        month_list = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+            'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+        date_list = date.split('.')
+        return (day_list[int(date_list[0]) - 1] + ' ' +
+            month_list[int(date_list[1]) - 1])
+    
+
+    def time_to_words(self, time):
+        time_list = time.split(':')
+        if time_list[1][0] == '0':
+            time_list[1] = ' ноль ' + time_list[1][1:]
+        return time_list[0] + ' ' + time_list[1]
+
+    def main(self):
+        while None in self.status.values():
+            speech = next(self.listener.get_speech())
+            entities = self.parser.parse(speech)
+            for key in self.status:
+                if self.status[key] is None and entities[key] is not None:
+                    self.status[key] = entities[key]
+            print(self.status.values())
+
+        confirmation = f"Произведена {self.status['request']} \
+            к {self.status['doctor']}y \
+                на {self.date_to_words(self.status['date'])} \
+                    в {self.time_to_words(self.status['time'])}"
+        print(confirmation)
+        self.vocalize(confirmation, True)
+
+
+if __name__ == "__main__":
+    call = Engine()
+    call.main()
---
+++ STT/__init__.py
@@ -0,0 +1,1 @@
+from .STT_RealTime import Listener
